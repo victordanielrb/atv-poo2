6c63b48ebd7992531f63e113695e12e602629064
Victor Daniel <161987258+victordanielrb@users.noreply.github.com>
2025-06-30 16:49:23 -0300

    Delete atviv-atlantis directory

--- atviv-atlantis/src/atlantis-react/src/paginas/clientes.tsx
+++
@@ -1,9 +0,0 @@
-import MenuClientes from "../components/clientes/menuclientes";
-
-export default function Clientes() {
-    return (
-        <div className="flex flex-col items-center justify-center">
-           <MenuClientes />
-        </div>
-    );
-}
